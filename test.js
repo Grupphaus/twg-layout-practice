adf8bbf46b3c1b93cd9b33e1c34fca1316117861
Federico Pellegrini <fellegrini@gmail.com>
2020-02-25 03:55:50 -0300

    header functionality and dropdowns. modal trigger functionality

--- test.js
+++ test.js
@@ -1,1 +1,28 @@
-this.addEventListener('load', () => { console.log('0000') })
+this.addEventListener('load', () => { 
+    console.log('0000');
+
+    const MODAL_TRIGGER = document.getElementById('modal-trigger');
+    const MODAL = document.getElementById('modal');
+    const MODAL_MENU = document.getElementById('modal-menu');
+    const MODAL_BACKDROP = document.getElementById('modal-backdrop');
+
+    MODAL_TRIGGER.addEventListener('click', () => {
+        MODAL.style.display = 'block';
+        setTimeout(() => {
+            MODAL.style.opacity = '1';
+            MODAL_MENU.style.opacity = '1';
+            MODAL_MENU.classList.add('no-translate');
+            document.body.classList.add('modal-active');
+        }, 50);
+    });
+
+    MODAL_BACKDROP.addEventListener('click', () => {
+        MODAL.style.opacity = '0';
+        MODAL_MENU.classList.remove('no-translate');
+        document.body.classList.remove('modal-active');
+        setTimeout(() => {
+            MODAL_MENU.style.opacity = '0';
+            MODAL.style.display = 'none';
+        }, 150);
+    })
+})
